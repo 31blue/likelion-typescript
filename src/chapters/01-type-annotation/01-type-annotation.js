5414d0273c49c2e4a15f55c127fca6f378972dce
blue31 <minijh98@gmail.com>
2025-01-06 11:04:40 +0900

    [Feat] ts를 js로 고치기기

--- src/chapters/01-type-annotation/01-type-annotation.js
+++ src/chapters/01-type-annotation/01-type-annotation.js
@@ -6,7 +6,6 @@
 // - "이 함수는 boolean 타입을 반환한다."
 // - "이 함수는 2개의 number 타입을 전달 받고, number 타입을 반환한다."
 // - "이 객체는 colors 프로퍼티를 꼭 포함해야 하며, string 타입으로만 구성된 array 타입이어야 한다."
-
 // -----------------------------------------------------------------------------------------
 // 📌 변수 타입(Valiable Types) → `let variable:type = value`
 // -----------------------------------------------------------------------------------------
@@ -18,17 +17,12 @@
 // - OBJECT TYPES
 //   - object, array, function
 //   - enum, tuple, etc.
-
-// npx tsc src/chapters/01-type-annotation/01-type-annotation.ts
 // -----------------------------------------------------------------------------------------
-
 {
-  // 문자 타입(string type)
-  let message: string = `변수 타입은 변수 이름 뒤에 ':type'을 작성하는 것을 말하며 이를 "타입 에너테이션"이라 부릅니다.`;
-
-  // 숫자 타입(number type)
-  let currentYear: number = 2022;
-
-  // 불리언 타입(boolean type)
-  let isWatch: boolean = false;
+    // 문자 타입(string type)
+    var message = "\uBCC0\uC218 \uD0C0\uC785\uC740 \uBCC0\uC218 \uC774\uB984 \uB4A4\uC5D0 ':type'\uC744 \uC791\uC131\uD558\uB294 \uAC83\uC744 \uB9D0\uD558\uBA70 \uC774\uB97C \"\uD0C0\uC785 \uC5D0\uB108\uD14C\uC774\uC158\"\uC774\uB77C \uBD80\uB985\uB2C8\uB2E4.";
+    // 숫자 타입(number type)
+    var currentYear = 2022;
+    // 불리언 타입(boolean type)
+    var isWatch = false;
 }
